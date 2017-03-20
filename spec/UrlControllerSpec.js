06e493acabf8bb94d3a534d3ba9efa7dec27ade9
Malory martin <gueishe@gmail.com>
2017-03-20 14:50:43 +0100

    init(some base for spec)

--- spec/UrlControllerSpec.js
+++ spec/UrlControllerSpec.js
@@ -1,3 +1,13 @@
-describe('UrlController', () => {
+const urlCtrl = require('app/controllers/UrlController.js');
 
-})
+const methodPrefix = "=> ";
+const messagePrefix = "-- ";
+
+describe("UrlController", () => {
+    // Method UT
+    describe(methodPrefix + "", () => {
+        // Test case - duplicate this for multiple tests
+        it(messagePrefix + "", next => {
+        });
+    });
+});
--- spec/UserControllerSpec.js
+++ spec/UserControllerSpec.js
@@ -1,3 +1,13 @@
-describe('UserController', () => {
+const userCtrl = require('app/controllers/UserController.js');
 
-})
+const methodPrefix = "=> ";
+const messagePrefix = "-- ";
+
+describe("UserController", () => {
+    // Method UT
+    describe(methodPrefix + "", () => {
+        // Test case - duplicate this for multiple tests
+        it(messagePrefix + "", next => {
+        });
+    });
+});
