3a60dea5b1bf6fa319f05c9e15105b7a1de97636
Malory martin <gueishe@gmail.com>
2017-03-20 16:09:03 +0100

    spec(Add usefull method name to the test cases ;))

--- spec/UrlServiceSpec.js
+++ spec/UrlServiceSpec.js
@@ -5,39 +5,47 @@ const messagePrefix = "-- ";
 
 describe("UrlService", () => {
     describe(methodPrefix + "addUrl", () => {
-        it(messagePrefix + "http://blabla.com", next => {
+        it(messagePrefix + "Correct url", next => {
             let res = UrlService.addUrl("http://blabla.com");
             expect(res).toBeTruthy();
             next();
         });
-        it(messagePrefix + "htt://blabla.com", next => {
+        it(messagePrefix + "Missing p in http", next => {
             let res = UrlService.addUrl("htt://blabla.com");
             expect(res).toBeFalsy();
             next();
         });
-        it(messagePrefix + "http//blabla.com", next => {
+        it(messagePrefix + "Missing : in http://", next => {
             let res = UrlService.addUrl("http//blabla.com");
             expect(res).toBeFalsy();
             next();
         });
-        it(messagePrefix + "http:blabla.com", next => {
+        it(messagePrefix + "Missing // in http://", next => {
             let res = UrlService.addUrl("http:blabla.com");
             expect(res).toBeFalsy();
             next();
         });
-        it(messagePrefix + "http://blabla", next => {
+        it(messagePrefix + "Missing .com", next => {
             let res = UrlService.addUrl("http://blabla");
             expect(res).toBeFalse();
             next();
         });
-        it(messagePrefix + "", next => {
+        it(messagePrefix + "Url is empty", next => {
             let res = UrlService.addUrl("");
             expect(res).toBeFalse();
             next();
         });
-        it(messagePrefix + "blabla", next => {
+        it(messagePrefix + "Missing http:// and .com", next => {
             let res = UrlService.addUrl("blabla");
             expect(res).toBeFalsy();
+            next();
+        });
+    });
+
+    describe(methodPrefix + "getUrl", () => {
+        it(messagePrefix + "http://blabla.com", next => {
+            let res = UrlService.addUrl("http://blabla.com");
+            expect(res).toBeTruthy();
             next();
         });
     });
