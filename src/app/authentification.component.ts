88a0f85217a0aa3b5d1a1080fca3d8acdd564f4d
Théo Turpin <theo.pierre@hotmail.fr>
2017-03-20 16:20:39 +0100

    Authentification Component

---
+++ src/app/authentification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './authentification.component.html',
+  styleUrls: ['../../public/css/materialize.min.css']
+})
+export class AuthentificationComponent {
+  title = 'Authentification';
+}
