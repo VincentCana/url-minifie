809d7cfc1c68a5d78c6d3181523f77e021dbace9
math <mathob.jehanno@hotmail.fr>
2017-03-20 16:00:17 +0100

    password validator unit test

--- spec/UserServiceSpec.js
+++ spec/UserServiceSpec.js
@@ -1,13 +1,42 @@
 const userCtrl = require('app/services/UserService.js');
 
+
+
 const methodPrefix = "=> ";
 const messagePrefix = "-- ";
 
 describe("UserService", () => {
     // Method UT
-    describe(methodPrefix + "", () => {
+
+    let password = userCtrl.passwordValidator();
+
+    describe(methodPrefix + "Password Validator", () => {
         // Test case - duplicate this for multiple tests
-        it(messagePrefix + "", next => {
-        });
-    });
+      it(messagePrefix + "Password is ok", next => {
+          expect(password).toBeTruthy();
+          next();
+      });
+      it(messagePrefix + "Password is not long enought", next => {
+          expect(password).toBeFalsy();
+          next();  
+      });
+      it(messagePrefix + "Password has no capitalcase character", next => {
+          expect(password).toBeFalsy();
+          next();
+      });
+      it(messagePrefix + "Password has no number", next => {
+          expect(password).toBeFalsy();
+          next();
+      });
+      it(messagePrefix + "Password has no lowercase character", next => {
+          expect(password).toBeFalsy();
+          next();
+      });
+      it(messagePrefix + "Password has no special character", next => {
+          expect(password).toBeFalsy();
+          next();
+      });
+  });
+  
+
 });
