6b3ff76c6849d896c06142433b149e824b3daa21
TournayG <tournayg@gmail.com>
2017-03-21 14:42:16 +0100

    confirmPasswordValidator checked

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,31 +1,31 @@
 const UserController = require ('../controllers/UserController');
 
 class UserService {
-    emailValidator(mail){
+    emailValidator(mail) {
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(mail);
     }
 
     passwordValidator(password) {
         let RegExp = /(?=.{5,})(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!#?$])/;
-        if(RegExp.test(password))
-        {
-            return(true);
+        if (RegExp.test(password)) {
+            return (true);
         }
-        else
-        {
-            return(false);
+        else {
+            return (false);
         }
 
     }
 
     confirmPasswordValidator(password, confirmpassword) {
-        if (password != null || password != ''){
-            if (password === confirmpassword){
+        if (password !== undefined && password !== '') {
+            if (password === confirmpassword) {
                 return true;
-            }else {
+            } else {
                 return false;
             }
+        } else {
+            return false;
         }
     }
 }
