d1b724436435a536ed3d69a564d81268c18630ad
TournayG <tournayg@gmail.com>
2017-03-21 16:33:06 +0100

    UserController checked

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -5,23 +5,16 @@ class UserController {
         let post = req.body;
         let mail = UserService.emailValidator(post.email);
         let password = UserService.passwordValidator(post.password);
-        let confirmPassword = UserService.confirmPasswordValidator(post.confirmPassword)
-
-
+        let confirmPassword = UserService.confirmPasswordValidator(post.confirmPassword);
 
         res.send({
             mail: mail,
             password: password,
             confirmPassword: confirmPassword
         });
-
-
-
     }
 
-    connect(req, res) {
-        res.send('yop connect');
-    }
+
 
     connect(req, res) {
         res.send('coucou')
