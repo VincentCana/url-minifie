915d9ad720a01b847420173dd54294a9584085ff
TournayG <tournayg@gmail.com>
2017-03-21 16:10:37 +0100

    starting UserController

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -2,7 +2,18 @@ const UserService = require('../services/UserService');
 
 class UserController {
     register(req, res) {
-        res.send('coucou register');
+        let post = req.body;
+        let mail = UserService.emailValidator(post.email);
+        let password = UserService.passwordValidator(post.password);
+        let confirmPassword = UserService.confirmPasswordValidator(post.confirmPassword)
+
+
+
+        res.send({
+            mail: mail,
+            password: password,
+            confirmPassword: confirmPassword
+        });
 
 
 
