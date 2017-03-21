d40ac928d9dc7c67d343ef3cde002deabb7d1120
BlackYoup <a.lefebvre@outlook.fr>
2017-03-21 14:05:32 +0100

    Basic connect

--- src/app/authentification.component.ts
+++ src/app/authentification.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { Router } from '@angular/router';
+
+const API_BASE_URL = "https://imie-api-mock.cleverapps.io";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AuthentificationComponent {
+  constructor(private http: Http, private router: Router){}
   title = 'Authentification';
+
+  email = null;
+  password = null;
+
+  onSubmit() {
+    this.login();
+  }
+
+  login() {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const requestOptions = new RequestOptions({ headers: headers });
+
+    this
+      .http
+      .post(`${API_BASE_URL}/connect`, { email: this.email, password: this.password }, requestOptions)
+      .toPromise()
+      .then(res => res.json())
+      .then(user => {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.router.navigateByUrl('/dashboard');
+      })
+      .catch(e => console.error(e));
+  }
 }
