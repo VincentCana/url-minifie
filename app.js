0402043d06816895b19bdf1621dbdc80e887de15
Vincent <vincent.canarelli@gmail.com>
2017-03-21 15:26:57 +0100

    'dbok'

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const urlRoute = require('./app/routes/urlRoute')
-const userRoute = require('./app/routes/userRoute')
+const urlRoute = require('./app/routes/urlRoute');
+const userRoute = require('./app/routes/userRoute');
 
 app.use('/', urlRoute);
 app.use('/', userRoute);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+mongoose.connect('mongodb://localhost:27017/urlMinifie', (err) => {
+    console.log(err);
+
+    app.listen(3000, function () {
+      console.log('Example app listening on port 3000!');
+    });
 });
