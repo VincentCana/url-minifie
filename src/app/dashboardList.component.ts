7462ff907de87db283fd4c11291cb35fc2b04a6b
BlackYoup <a.lefebvre@outlook.fr>
2017-03-22 11:38:03 +0100

    Basic delete of urls

--- src/app/dashboardList.component.ts
+++ src/app/dashboardList.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+const API_BASE_URL = "https://imie-api-mock.cleverapps.io";
 
 @Component({
   selector: 'dashboard-list',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class DashboardListComponent {
-  urls = localStorage.getItem('urls') || [];
+  constructor(private http: Http){};
+
+  urls = localStorage.getItem('urls') || [{url: "test.com", urlMinifie: "b.com"}];
+
+  deleteUrl(event, url) {
+    this
+      .http
+      .delete(`${API_BASE_URL}/urls/${encodeURIComponent(url.url)}`, {})
+      .toPromise()
+      .then(() => console.log("done"))
+      .catch(e => console.error(e));
+
+  }
 }
